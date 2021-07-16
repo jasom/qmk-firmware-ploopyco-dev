b8dd5b94c05138b75a38203c4b151c032b7220b7
ploopyco <git@ploopy.co>
2021-07-16 02:01:02 +0100

    more changes

--- keyboards/ploopyco/device_chaining/device_chaining.c
+++ keyboards/ploopyco/device_chaining/device_chaining.c
@@ -16,66 +16,70 @@
 
 #include "quantum.h"
 #include "wait.h"
+#include "lufa.h"
 
 #include "device_chaining.h"
 
-#define MASTER_DATA_PIN_OUT B5    // pin 29
-#define MASTER_DATA_PIN_IN B6     // pin 30
-#define MASTER_LED_PIN D6         // pin 26
-#define MASTER_BUTTON_PIN D5      // pin 22
+// NOTE: These are NOT SPI pin definitions.
+#define MOSI_PIN B5           // pin 29
+#define MISO_PIN B6           // pin 30
 
-#define SLAVE_DATA_PIN_IN B5      // pin 29
-#define SLAVE_DATA_PIN_OUT B6     // pin 30
-#define SLAVE_LED_PIN D6          // pin 26
-#define SLAVE_BUTTON_PIN D5       // pin 22
+#define REMOTE_LED_PIN D6     // pin 26
+#define REMOTE_BUTTON_PIN D5  // pin 22
+#define LOCAL_LED_PIN C7      // pin 32
+#define LOCAL_BUTTON_PIN C6   // pin 31
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { };
 
-/*** MASTER CODE STARTS HERE ***************************************/
+bool isMaster = false;
 
 void keyboard_pre_init_kb(void) {
-    setPinOutput(MASTER_DATA_PIN_OUT);
-    setPinInputHigh(MASTER_DATA_PIN_IN);
-    setPinOutput(MASTER_LED_PIN);
-    setPinInput(MASTER_BUTTON_PIN);
+    setPinOutput(REMOTE_LED_PIN);
+    setPinOutput(LOCAL_LED_PIN);
+    setPinInput(REMOTE_BUTTON_PIN);
+    setPinInput(LOCAL_LED_PIN);
+}
+
+void keyboard_post_init_user(void) {
+    wait_ms(50);
+    if (USB_DeviceState == DEVICE_STATE_Configured) {
+        isMaster = true;
+        setPinOutput(MOSI_PIN);
+        setPinInputHigh(MISO_PIN);
+    } else {
+        setPinOutput(MISO_PIN);
+        setPinInputHigh(MOSI_PIN);
+    }
 }
 
 void housekeeping_task_user(void) {
-    uint8_t p = readPin(MASTER_BUTTON_PIN);
-    if (p == 1)
-        writePinHigh(MASTER_DATA_PIN_OUT);
-    if (p == 0)
-        writePinLow(MASTER_DATA_PIN_OUT);
+    uint8_t p = readPin(REMOTE_BUTTON_PIN);
+    if (p == 1) {
+        if (isMaster)
+            writePinHigh(MOSI_PIN);
+        else
+            writePinHigh(MISO_PIN);
+    } else {
+        if (isMaster)
+            writePinLow(MOSI_PIN);
+        else
+            writePinLow(MISO_PIN);
+    }
 
-    uint8_t q = readPin(MASTER_DATA_PIN_IN);
+    uint8_t q = readPin(LOCAL_BUTTON_PIN);
     if (q == 1)
-        writePinHigh(MASTER_LED_PIN);
+        writePinHigh(LOCAL_LED_PIN);
     if (q == 0)
-        writePinLow(MASTER_LED_PIN);
+        writePinLow(LOCAL_LED_PIN);
+
+    uint8_t r;
+    if (isMaster)
+        r = readPin(MISO_PIN);
+    else
+        r = readPin(MOSI_PIN);
+
+    if (r == 1)
+        writePinHigh(REMOTE_LED_PIN);
+    if (r == 0)
+        writePinLow(REMOTE_LED_PIN);
 }
-
-
-/*** SLAVE CODE STARTS HERE ****************************************
-
-void keyboard_pre_init_kb(void) {
-    setPinOutput(SLAVE_DATA_PIN_OUT);
-    setPinInputHigh(SLAVE_DATA_PIN_IN);
-    setPinOutput(SLAVE_LED_PIN);
-    setPinInput(SLAVE_BUTTON_PIN);
-}
-
-void housekeeping_task_user(void) {
-    uint8_t p = readPin(SLAVE_BUTTON_PIN);
-    if (p == 1)
-        writePinHigh(SLAVE_DATA_PIN_OUT);
-    if (p == 0)
-        writePinLow(SLAVE_DATA_PIN_OUT);
-
-    uint8_t q = readPin(SLAVE_DATA_PIN_IN);
-    if (q == 1)
-        writePinHigh(SLAVE_LED_PIN);
-    if (q == 0)
-        writePinLow(SLAVE_LED_PIN);
-}
-*/
-
